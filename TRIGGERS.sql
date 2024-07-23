41df9a4cc2e2036ee88ef130aa8ea195f44a20ca
JhoanSLC <JhoanSLC4@gmail.com>
2024-07-23 11:54:23 -0500

    feat: :sparkles: Trigger validarCampsObligatorios insert
    Se crea el primer trigger que se encarga de validar
    que el usuario ingrese los campos obligatorios en
    los inserts ya que no pueden ser nulos

---
+++ TRIGGERS.sql
@@ -0,0 +1,36 @@
+-- ······· FLUJO PRINCIPAL ·········· --
+
+    -- ·· Trigger para validar que los campos obligatorios sean llenados por el usuario ·· --
+
+DELIMITER $$ 
+
+CREATE TRIGGER validar_campos_obligatorios
+BEFORE 
+    INSERT OR UPDATE
+ON 
+    bicicleta
+FOR EACH ROW
+BEGIN
+
+    IF NEW.modelo IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El campo modelo es OBLIGATORIO y no puede ser NULL';
+    END IF;
+
+    IF NEW.marca IS NULL THEN 
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El campo marca es OBLIGATORIO y no puede ser NULL';
+    END IF;
+
+    IF NEW.precio IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El campo precio es OBLIGATORIO y no puede ser NULL';
+    END IF;
+
+    IF NEW.stock IS NULL THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El campo stock es OBLIGATORIO y no puede ser NULL';
+    END IF;
+END$$
+
+DELLIMITER ;
