279d02188a47b95fea8ec47a40b906ae563df2e8
JhoanSLC <JhoanSLC4@gmail.com>
2024-07-24 12:52:57 -0500

    feat: :sparkles: Caso 5 terminado (FALTA TEST) :sparkles:
    Se terminan los triggers y procedures
    necesarios para el caso de uso 5 pero
    no se hicieron los testeos necesarios

--- CasosDeUso/caso5/procedures.sql
+++ CasosDeUso/caso5/procedures.sql
@@ -1,34 +1,65 @@
-DELIMITER $$
+/* 
+    Este procedure se encarga de registrar las compras hechas por el cliente 
+        RECIBE: Id del proveedor
+        DEVUELVE: Id de la compra que se acaba de registrar
 
-CREATE PROCEDURE registrarDetalleCompra(
-    IN p_compraId INT,
-    IN p_repuestoId INT,
-    IN p_cantidad INT,
-    IN p_precioUnitario DECIMAL(10,2)
+*/
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS registrarCompra$$
+CREATE PROCEDURE registrarCompra(
+    IN inProveedorId INT,
+    OUT outCompraId INT
 )
 BEGIN
-    -- Insertar el detalle de compra
-    INSERT INTO detalleCompra (compraId, repuestoId, cantidad, precioUnitario)
-    VALUES (p_compraId, p_repuestoId, p_cantidad, p_precioUnitario);
-    
-    -- Actualizar el stock del repuesto
-    UPDATE repuesto
-    SET stock = stock + p_cantidad
-    WHERE id = p_repuestoId;
-END$$
+    -- Crear una nueva compra --
+    INSERT INTO compras (fecha, proveedorId, total)
+    VALUES (NOW(), inProveedorId, 0);
 
+    -- Obtener el ID de la nueva compra creada --
+    SET CompraID = LAST_INSERT_ID();
+END $$
 DELIMITER ;
 
+
+/*
+    Este procedure se encarga de registrar los detalles de la compra
+        RECIBE: El id de la compra que se va a detallar (inCompraId)
+                El id del repuesto que se compra (inRepuestoId)
+                La cantidad de ese producto que se compra (inCantidad)
+*/
+
 DELIMITER $$
-
-DROP TRIGGER IF EXISTS actualizar_stock_repuestos$$
-
-CREATE TRIGGER actualizar_stock_repuestos 
-AFTER INSERT ON detalleCompra
-FOR EACH ROW
+DROP PROCEDURE registrarDetalleCompra$$
+CREATE PROCEDURE registrarDetalleCompra(
+    IN inCompraId INT,
+    IN inRepuestoId INT,
+    IN inCantidad INT,
+)
 BEGIN
-    -- Llamar al procedimiento para actualizar el stock
-    CALL registrarDetalleCompra(NEW.compraId, NEW.repuestoId, NEW.cantidad, NEW.precioUnitario);
-END$$
+    DECLARE totalDetalle DECIMAL(10, 2);
+    DECLARE precioUnitario DECIMAL(10,2);
 
+    -- Obtener el precio unitario del repuesto --
+    SELECT precio INTO precioUnitario
+    FROM repuesto
+    WHERE id = inRepuestoId;
+
+    -- Calcular el total del detalle --
+    SET totalDetalle = precioUnitario * inCantidad;
+
+    -- Insertar el detalle de la compra
+    INSERT INTO DetallesDeCompras (compraId, repuestoId, cantidad, precioUnitario)
+    VALUES (inCompraId, inRepuestoId, inCantidad, precioUnitario);
+
+    -- Actualizar el stock de los repuestos
+    UPDATE repuesto
+    SET stock = stock + inCantidad
+    WHERE id = inRepuestoId;
+
+    -- Actualizar el total de la compra
+    UPDATE compra
+    SET total = total + totalDetalle
+    WHERE id = inCompraId;
+END $$
 DELIMITER ;
--- CasosDeUso/caso5/triggers.sql
+++ CasosDeUso/caso5/triggers.sql
@@ -1,12 +1,44 @@
 DELIMITER $$
-
-CREATE TRIGGER actualizar_stock_repuestos 
-AFTER INSERT ON detalleCompra
-FOR EACH ROW
-BEGIN
-    UPDATE repuesto
-    SET stock = stock + NEW.cantidad
-    WHERE id = NEW.repuestoId;
+DROP TRIGGER IF EXISTS stockNegativoRepuestoIns$$
+CREATE TRIGGER stockRepuestoIns
+BEFORE INSERT ON repuesto
+FOR EACH ROW 
+BEGIN 
+    IF NEW.stock < 0 OR NEW.precio < 0 THEN 
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El "STOCK" y el "PRECIO" no pueden ser negativos';
+    END IF;
 END$$
+DELIMITER ;
 
+DELIMITER $$
+DROP TRIGGER IF EXISTS stockNegativoRepuestoIns$$
+CREATE TRIGGER stockRepuestoIns
+BEFORE UPDATE ON repuesto
+FOR EACH ROW 
+BEGIN 
+    IF NEW.stock < 0 OR NEW.precio < 0 THEN 
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El "STOCK" y el "PRECIO" no pueden ser negativos';
+    END IF;
+END$$
+DELIMITER ;
+
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS verificarStockAntesDeCompra$$
+CREATE TRIGGER verificarStockAntesDeCompra
+BEFORE INSERT ON detalleCompra
+FOR EACH ROW
+BEGIN   
+    DECLARE stockActual INT;
+    
+    -- Obtener el stock actual del repuesto --
+    SELECT stock INTO stockActual
+    FROM repuesto
+    WHERE id = NEW.repuestoId;
+    
+    -- Verificar que el stock sea suficiente --
+    IF stockActual < NEW.cantidad THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Stock insuficiente para la bicicleta seleccionada';
+    END IF;
+END $$
 DELIMITER ;
