0abecaad2a96fdf249f9b746b668a4cd295408ab
JhoanSLC <JhoanSLC4@gmail.com>
2024-07-24 07:18:09 -0500

    feat: :white_check_mark: Caso1 probado y funcionadno :white_check_mark:

--- CasosDeUso/caso1/triggers.sql
+++ CasosDeUso/caso1/triggers.sql
@@ -7,8 +7,8 @@ CREATE TRIGGER stockNegativoIns
 BEFORE INSERT ON bicicleta
 FOR EACH ROW
 BEGIN
-    IF NEW.stock < 0 THEN -- Si se inserta un valor negativo para el stock --
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El stock no puede ser negativo'; -- Enviar un mensaje --
+    IF NEW.stock < 0 OR NEW.precio < 0 THEN -- Si se inserta un valor negativo para el stock --
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El "STOCK" y el "PRECIO" no pueden ser negativo'; -- Enviar un mensaje --
     END IF;
 END $$
 DELIMITER ;
@@ -21,8 +21,8 @@ CREATE TRIGGER stockNegativoUpd
 BEFORE UPDATE ON bicicleta
 FOR EACH ROW
 BEGIN
-    IF NEW.stock < 0 THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El stock no puede ser negativo';
+    IF NEW.stock < 0 OR NEW.precio < 0 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El "STOCK" y el "PRECIO" no pueden ser negativo';
     END IF;
 END $$
 DELIMITER ;
