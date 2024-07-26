45d89e2bf0e94a7ce52d03da315f18665f548d6e
JhoanSLC <JhoanSLC4@gmail.com>
2024-07-26 11:18:01 -0500

    :white_check_mark: caso1 de resumen testeado :white_check_mark:

---
+++ CasosDeUso/casosDeResumen/caso1.sql
@@ -0,0 +1,24 @@
+/* 
+Este procedimiento se encarga de devolver
+el total de ventas que se realizaron en
+un mes especifico 
+
+    RECIBE:
+            * El mes en NÚMERO del cual se calculara el total
+                de ventas
+*/
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ventasPorMes$$
+CREATE PROCEDURE ventasPorMes(
+    in inMes INT
+)
+BEGIN
+
+    SELECT SUM(total) AS totalVentas
+    FROM venta
+    WHERE MONTH(fecha) = inMes
+    GROUP BY MONTH(fecha);
+    
+END$$
+DELIMITER ;
