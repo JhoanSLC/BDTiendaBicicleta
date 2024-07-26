d10f9b0ff92db8f339a6d08cf297960c86715e54
JhoanSLC <JhoanSLC4@gmail.com>
2024-07-26 11:01:08 -0500

    :white_check_mark: caso 8 IP testeado :white_check_mark:

--- CasosDeUso/implementarProcedures/caso8/procedures.sql
+++ CasosDeUso/implementarProcedures/caso8/procedures.sql
@@ -1,7 +1,14 @@
-    IF NEW.stock < 0 OR NEW.precio < 0 THEN -- Si se inserta un valor negativo para el stock --
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El "STOCK" y el "PRECIO" no pueden ser negativo'; -- Enviar un mensaje --
-    END IF;
+/*
+    Este procedimiento se encarga de verificar el stock actual de una bicicleta
+    comparandolo con la cantidad que se desea vendar, devolviendo un mensaje
+    de error en caso de no haber y devolviendo un affected row en caso de 
+    contar con el stock suficiente
 
+        RECIBE:
+                * Id de la bicicleta a verificar
+                * Cantidad de esa bicicleta que se vende
+    
+*/
 
 DELIMITER $$
 DROP PROCEDURE IF EXISTS verificarStockBicicleta$$
@@ -10,5 +17,17 @@ CREATE PROCEDURE verificarStockBicicleta(
     IN inCantidad INT
 )
 BEGIN 
-    DECLARE stock INT;
+    DECLARE stockActual INT;
+
+    SELECT b.stock INTO stockActual
+    FROM bicicleta b
+    WHERE b.id = inBicicletaId;
     
+    IF stockActual < inCantidad THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Stock insuficiente para esta bicicleta';
+    END IF;
+END $$
+DELIMITER ;
+
+    
+-- TESTEADO Y FUNCIONANDO --
